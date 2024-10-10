1df4221aa87a600cc6a59358305660e458c36b01
rider <ridebus82@nate.com>
2024-10-10 20:31:38 +0900

    2410010 asdjgogogo

--- front/src/routes/(app)/schedule/+page.svelte
+++ front/src/routes/(app)/schedule/+page.svelte
@@ -1,4 +1,25 @@
+<script>
+    import FullCalendar from "svelte-fullcalendar";
+    import daygridPlugin from "@fullcalendar/daygrid";
+
+    let options = {
+        dateClick: (event) => alert("date click! " + event.dateStr),
+        events: [
+            { title: "09:00~12:00 김예지님", date: "2024-09-30" },
+            { title: "event 2", date: "2024-09-30" },
+            { title: "event 3", date: "2024-09-30" },
+        ],
+        initialView: "dayGridMonth",
+        plugins: [daygridPlugin],
+    };
+
+    
+</script>
+
+<!-- <svelte:window on:load={() => console.log('DOM loaded')}> -->
+
 <div class="max-w-[1000px] mx-auto px-2 pt-28">
+    <FullCalendar {options} />
     <div class="text-center text-lg font-semibold">예약현황 페이지</div>
 
     <div class="text-sm">
